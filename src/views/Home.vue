47763efeeee0c1e4fb314dc7f4cbae5d00c2f481
duong.ngoc.phuc <SUN-ASTERISK\duong.ngoc.phuc@b122561-pc.sun-asterisk.com>
2021-09-04 15:10:41 +0700

    add blog post

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,43 @@
 <template>
   <div class="home">
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost
+      v-for="post in sampleBlogPost"
+      :key="post.blogHTML"
+      :post="post"
+    />
   </div>
 </template>
 
 <script>
+import BlogPost from "../components/BlogPost";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    BlogPost,
+  },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "welcome",
+        blogPost:
+          "Weekly blog articles with all things programming including HTML, CSS, Javascript and more. Register today to nerver miss posts",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a Fillter Title!",
+          blogHTML: "This is fillter blog post title!",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "This is a Fillter Title!",
+          blogHTML: "This is fillter blog post title!",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+    };
+  },
 };
 </script>
